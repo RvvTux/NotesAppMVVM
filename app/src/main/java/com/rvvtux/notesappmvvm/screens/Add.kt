0c76ccd710f6d0860b0f250605928c07a9cb87e1
RvvTux <103306592+RvvTux@users.noreply.github.com>
2022-07-11 20:54:38 +0600

    lesson 1

---
+++ app/src/main/java/com/rvvtux/notesappmvvm/screens/Add.kt
@@ -0,0 +1,9 @@
+package com.rvvtux.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun Add(navController: NavHostController) {
+}
---
+++ app/src/main/java/com/rvvtux/notesappmvvm/screens/Main.kt
@@ -0,0 +1,8 @@
+package com.rvvtux.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Main(navController: NavHostController) {
+}
